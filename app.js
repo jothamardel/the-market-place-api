b0349c6147de63984066474b2ad80cdf18eec5be
Bee <ardelmbiplang@gmail.com>
2020-07-15 14:57:44 +0100

    Heroku Update

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ exports.knex = require('knex')({
   client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: true
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
 });
 
