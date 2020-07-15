1aa534ee6ac445d641f5b908266af545bb6e733e
Bee <ardelmbiplang@gmail.com>
2020-07-15 14:18:58 +0100

    Added PORT as environmental variable

--- app.js
+++ app.js
@@ -24,4 +24,5 @@ app.use('/api/auth', adminRoute);
 app.use('/api', businessRoute);
 app.use(get404);
 
-app.listen(3000, () => console.log('Server running on PORT 3000.'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
