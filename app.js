40cc96a26e9b4048fc809c7e89ece05bcf173c43
jothamardel <ardelmbiplang@gmail.com>
2020-08-06 16:42:56 +0100

    CORS configuration...

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app = express();
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   next();
 // });
-app.use(cors(corsOptionsDelegate));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
