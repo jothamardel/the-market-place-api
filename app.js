b50cbf017214b365092b7137e7a3ef10a0b5edc3
jothamardel <ardelmbiplang@gmail.com>
2020-08-06 16:37:13 +0100

    CORS configuration

--- app.js
+++ app.js
@@ -38,28 +38,28 @@ dotenv.config();
 
 // console.log(process.env.NODE)
 
-// const whitelist = ['http://localhost:3001']
+const whitelist = ["https://the-market-place.vercel.app/", "http://localhost:3001"]
 
-// const corsOptionsDelegate = (req, callback) => {
-//   let corsOptions;
-//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
-//     // reflect (enable) the requested origin in the CORS response
-//     corsOptions = { origin: true, credentials: true };
-//   } else {
-//     corsOptions = { origin: false, credentials: true }; // disable CORS for this request
-//   }
-//   callback(null, corsOptions); // callback expects two parameters: error and options
-// };
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: true, credentials: true };
+  } else {
+    corsOptions = { origin: false, credentials: true }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 
 
 
 app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://the-market-place.vercel.app/"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://the-market-place.vercel.app/"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
