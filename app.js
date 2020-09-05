68b98a4d2c4a7a11502c4d8de26802499cd83a3b
jothamardel <ardelmbiplang@gmail.com>
2020-09-05 12:02:46 +0100

    CORS COnfig

--- app.js
+++ app.js
@@ -11,16 +11,28 @@ const { get404 } = require('./controllers/error.controller');
 dotenv.config();
 
 
-const corsOptions = {
-  origin: 'https://the-market-place.vercel.app'
+// const corsOptions = {
+//   origin: 'https://the-market-place.vercel.app'
+// }
+
+var whitelist = ['https://the-market-place.vercel.app'];
+
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
 app = express();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(cors('no-cors'));
+  app.use(cors(corsOptionsDelegate));
 } else {
-  app.use(cors());
+  app.use(cors(corsOptionsDelegate));
 }
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
