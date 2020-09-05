04d453b1f69e07cee7c7c0a80cf94bee83677f2f
jothamardel <ardelmbiplang@gmail.com>
2020-09-05 13:06:32 +0100

    Fixing Endpoint

--- controllers/create-user.controller.js
+++ controllers/create-user.controller.js
@@ -27,7 +27,7 @@ exports.createNewUser = (req, res) => {
 exports.loginUser = (req, res) => {
   const { number, password } = req.body;
 
-  CreateUser.find({ number: number, password: Number(password) })
+  CreateUser.find({ number: number, password: password })
     .then((data) => {
       if (data.length) {
         const userInfo = {
